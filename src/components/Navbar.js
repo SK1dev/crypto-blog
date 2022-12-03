9686c8695b93d45b22b1cbc9193d86a64b2cb8b2
SK374 <sk374@protonmail.com>
2022-12-03 21:59:17 +0100

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className='Navbar'>
+        <h1>Coin Hub</h1>
+        <div className="link">
+            <a href='/'>Home</a>
+            <a href='create'>New Blog</a>
+        </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
