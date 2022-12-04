0091d43ac62ef5e22e91d7a8851197b81d35d5c0
SK374 <sk374@protonmail.com>
2022-12-04 13:53:54 +0100

    Create custom hook, error handling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,18 @@
 import { useState, useEffect } from 'react'
 import BlogList from './BlogList';
+import UseFetch from './useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "FTX's Collapse - What really happened?", body: 'lorem ipsum...', author: 'fabian', id: 1 },
-    { title: 'Will Crypto go back up?', body: 'lorem ipsum...', author: 'simon', id: 2 },
-    { title: 'How to set up a Wallet', body: 'lorem ipsum...', author: 'milly', id: 3 }
-  ])
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  useEffect(() => {})
+  const { data: blogs, isPending, error } = UseFetch('http://localhost:8000/blogs');
 
   return (
     <div className='home'>
-        <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete}/>
-        <BlogList blogs={blogs.filter((blog) => blog.author === 'milly' )} title="Milly's blogs"/>
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
+      {/* {blogs && <BlogList blogs={blogs.filter((blog) => blog.author === 'milly' )} title="Milly's blogs"/>} */}
     </div>
   );
 }
- 
+
 export default Home;
